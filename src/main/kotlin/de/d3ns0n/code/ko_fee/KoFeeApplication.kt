795cf067b3c2a1389c02f515516dc47b3c1fb408
Dennis Lotz <lotz@365farmnet.com>
2024-12-28 11:43:58 +0100

    init(): ramp up project

---
+++ src/main/kotlin/de/d3ns0n/code/ko_fee/KoFeeApplication.kt
@@ -0,0 +1,11 @@
+package de.d3ns0n.code.ko_fee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoFeeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KoFeeApplication>(*args)
+}
---
+++ src/test/kotlin/de/d3ns0n/code/ko_fee/KoFeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.d3ns0n.code.ko_fee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KoFeeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
